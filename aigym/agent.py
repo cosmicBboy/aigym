43b8a0085c6303ed984393a90fc0c077939f1763
cosmicBboy <niels.bantilan@gmail.com>
2025-08-17 20:54:23 -0400

    updates
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- aigym/agent.py
+++ aigym/agent.py
@@ -176,8 +176,11 @@ class Agent:
         if action["url"] and not action["url"].startswith("http"):
             action["url"] = urllib.parse.urljoin(f"{_url.scheme}://{_url.netloc}", action["url"])
 
-        if action["url"] and self._url_not_in_context(action["url"], observation.context):
-            raise InvalidActionError(f"url {action['url']} is not in the context. action: {action}")
+        try:
+            if action["url"] and self._url_not_in_context(action["url"], observation.context):
+                raise InvalidActionError(f"url {action['url']} is not in the context. action: {action}")
+        except ValueError as exc:
+            raise InvalidActionError(str(exc)) from exc
 
         return action, reasoning_trace
 
